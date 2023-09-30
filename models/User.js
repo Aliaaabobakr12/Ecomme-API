e680dc6dade13b48bfbf2d2cd81e01755b87ef65
Aliaaabobakr12 <aliaaabobakr12@gmail.com>
2023-09-30 18:40:34 +0300

    edit user's fields model

--- models/User.js
+++ models/User.js
@@ -1,54 +1,70 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
+const addressSchema = require("./addressSchema");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide name"],
-    minlength: 3,
-    maxlength: 50,
-  },
-  address: {
-    required: [true, "Please provide address"],
-    street: String,
-    city: String,
-    state: String,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: [true, "Please provide email"],
-    validate: {
-      validator: (mail) => {
-        return validator.isEmail(mail);
-      },
-      message: "Please provide a valid email",
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide name"],
+      minlength: 3,
+      maxlength: 50,
     },
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide password"],
-    minlength: 8,
-    select: false,
-  },
-  phoneNumber: {
-    type: String,
-    required: [true, "Please provide phone number"],
-    validate: {
-      validator: function (n) {
-        return /^\d{11}$/.test(n);
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Please provide email"],
+      validate: {
+        validator: (mail) => {
+          return validator.isEmail(mail);
+        },
+        message: "Please provide a valid email",
       },
-      message: "Please provide a valid number",
+    },
+    address: {
+      type: addressSchema,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide password"],
+      minlength: 8,
+      select: false,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (n) {
+          return /^\d{11}$/.test(n);
+        },
+        message: "Please provide a valid number",
+      },
+    },
+    secondryPhone: {
+      type: String,
+      required: false,
+      validate: {
+        validator: function (n) {
+          return /^\d{11}$/.test(n);
+        },
+        message: "Please provide a valid number",
+      },
+    },
+    userType: {
+      type: String,
+      required: true,
+      enum: ["customer", "admin", "owner"],
+    },
+    order: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Order",
+      required: [true, "Please provide the order ref"],
     },
   },
-  order: {
-    type: mongoose.Schema.ObjectId,
-    ref: "Order",
-    required: [true, "Please provide the order ref"],
-  },
-});
+  { timestamps: true }
+);
 
 UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
