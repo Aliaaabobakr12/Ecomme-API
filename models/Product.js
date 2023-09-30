a674f0a15fe1b95cdaff1356fe72071adc853474
Aliaaabobakr12 <aliaaabobakr12@gmail.com>
2023-09-30 19:06:58 +0300

    Add languages schema

--- models/Product.js
+++ models/Product.js
@@ -1,35 +1,22 @@
 const mongoose = require("mongoose");
 
+const langSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  description: {
+    type: String,
+    required: [true, "Please provide product's description"],
+    minlength: [1000, "Description shouldn't be less than 1000 charctares"],
+  },
+  info: String,
+});
+
 const ProductSchema = new mongoose.Schema(
   {
-    en: {
-      name: {
-        type: String,
-        required: [true, "Name is required"],
-        minlength: 3,
-        maxlength: 50,
-      },
-      description: {
-        type: String,
-        required: [true, "Please provide product's description"],
-        minlength: [1000, "Description shouldn't be less than 1000 charctares"],
-      },
-      info: String,
-    },
-    ar: {
-      name: {
-        type: String,
-        required: [true, "Name is required"],
-        minlength: 3,
-        maxlength: 50,
-      },
-      description: {
-        type: String,
-        required: [true, "Please provide product's description"],
-        minlength: [1000, "Description shouldn't be less than 1000 charctares"],
-      },
-      info: String,
-    },
     images: {
       type: String,
       required: true,
@@ -60,6 +47,8 @@ const ProductSchema = new mongoose.Schema(
       default: Date.now,
       required: false,
     },
+    en: langSchema,
+    ar: langSchema,
   },
   { timestamps: true }
 );
