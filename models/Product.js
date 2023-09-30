4fb15c793d482c5f40519d8eb48f592bde363a9d
Aliaaabobakr12 <aliaaabobakr12@gmail.com>
2023-09-30 18:43:24 +0300

    Edit product model

--- models/Product.js
+++ models/Product.js
@@ -1,37 +1,67 @@
 const mongoose = require("mongoose");
 
-const ProductSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: [true, "ID is required"],
-    unique: [true, "ID shoudln't be use from other products"],
+const ProductSchema = new mongoose.Schema(
+  {
+    en: {
+      name: {
+        type: String,
+        required: [true, "Name is required"],
+        minlength: 3,
+        maxlength: 50,
+      },
+      description: {
+        type: String,
+        required: [true, "Please provide product's description"],
+        minlength: [1000, "Description shouldn't be less than 1000 charctares"],
+      },
+      info: String,
+    },
+    ar: {
+      name: {
+        type: String,
+        required: [true, "Name is required"],
+        minlength: 3,
+        maxlength: 50,
+      },
+      description: {
+        type: String,
+        required: [true, "Please provide product's description"],
+        minlength: [1000, "Description shouldn't be less than 1000 charctares"],
+      },
+      info: String,
+    },
+    images: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Category",
+      required: [true, "Category is required"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "quantity is required"],
+    },
+    price: {
+      type: Number,
+      required: [true, "quantity is required"],
+    },
+    addedBy: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: {
+      type: Date,
+      default: Date.now,
+      required: false,
+    },
   },
-  name: {
-    type: String,
-    required: [true, "Name is required"],
-    minlength: 3,
-    maxlength: 50,
-  },
-  images: String,
-  category: {
-    type: mongoose.Schema.ObjectId,
-    ref: "Category",
-    required: [true, "Category is required"],
-  },
-  description: {
-    type: String,
-    required: [true, "Please provide product's description"],
-    minlength: [1000, "Description shouldn't be less than 1000 charctares"],
-  },
-  quantity: {
-    type: Number,
-    required: [true, "quantity is required"],
-  },
-  price: {
-    type: Number,
-    required: [true, "quantity is required"],
-  },
-  info: String,
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Product", ProductSchema);
