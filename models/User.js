bce32774d56eab50d49e33ae7a127203d79e5762
Aliaaabobakr12 <aliaaabobakr12@gmail.com>
2023-10-12 00:26:10 +0300

    some edits

--- models/User.js
+++ models/User.js
@@ -3,49 +3,58 @@ const bcrypt = require("bcryptjs");
 const validator = require("validator");
 const { addressSchema, phoneSchema } = require("./addressSchema");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide name"],
-    minlength: 3,
-    maxlength: 50,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: [true, "Please provide email"],
-    validate: {
-      validator: validator.isEmail,
-      message: "Please provide valid email",
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide name"],
+      minlength: 3,
+      maxlength: 50,
     },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Please provide email"],
+      validate: {
+        validator: validator.isEmail,
+        message: "Please provide valid email",
+      },
+    },
+    // address: {
+    //   type: addressSchema,
+    //   required: true,
+    // },
+    password: {
+      type: String,
+      required: [true, "Please provide password"],
+      minlength: 8,
+    },
+    //   phoneNumber: {
+    //     type: phoneSchema,
+    //     required: true,
+    //   },
+    userType: {
+      type: String,
+      required: true,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    verificationToken: String,
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verified: Date,
+    //   order: {
+    //     type: mongoose.Schema.ObjectId,
+    //     ref: "Order",
+    //     required: [true, "Please provide the order ref"],
+    //   },
+    // },
   },
-  // address: {
-  //   type: addressSchema,
-  //   required: true,
-  // },
-  password: {
-    type: String,
-    required: [true, "Please provide password"],
-    minlength: 8,
-  },
-  //   phoneNumber: {
-  //     type: phoneSchema,
-  //     required: true,
-  //   },
-  userType: {
-    type: String,
-    required: true,
-    enum: ["admin", "user"],
-    default: "user",
-  },
-  //   order: {
-  //     type: mongoose.Schema.ObjectId,
-  //     ref: "Order",
-  //     required: [true, "Please provide the order ref"],
-  //   },
-  // },
-  // { timestamps: true }
-});
+  { timestamps: true }
+);
 
 UserSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
