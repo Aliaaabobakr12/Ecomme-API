0da1a11298cd0e83fdf087122c353eaef63a8fa5
Aliaaabobakr12 <aliaaabobakr12@gmail.com>
2023-10-12 00:23:31 +0300

    provide verification token function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,13 @@
 const User = require("../models/User");
+const Token = require("../models/Token");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
-const { attachCookiesToResponse } = require("../utils/jwt");
-const { createTokenUser } = require("../utils/createTokenUser");
+const {
+  attachCookiesToResponse,
+  createTokenUser,
+  sendVerificationEmail,
+} = require("../utils");
+const crypto = require("crypto");
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -15,10 +20,45 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   const userType = isFirstAccount ? "admin" : "user";
 
-  const user = await User.create({ name, email, password, userType });
-  const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
-  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+  const verificationToken = crypto.randomBytes(40).toString("hex");
+  const user = await User.create({
+    name,
+    email,
+    password,
+    userType,
+    verificationToken,
+  });
+
+  const origin = "http://localhost:3000/";
+  await sendVerificationEmail({
+    name: user.name,
+    email: user.email,
+    verificationToken: user.verificationToken,
+    origin,
+  });
+  res.status(StatusCodes.CREATED).json({
+    msg: "Success! Please check your email to verify account",
+  });
+};
+
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Verification Failed");
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError("Verification Failed");
+  }
+
+  (user.isVerified = true), (user.verified = Date.now());
+  user.verificationToken = "";
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: "Email Verified" });
 };
 
 const login = async (req, res) => {
@@ -34,17 +74,48 @@ const login = async (req, res) => {
   if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError("Invalid Credentials");
   }
+  if (!user.isVerified) {
+    throw new CustomError.UnautherizedError("Please Verify your email");
+  }
   const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
+
+  // create refresh token
+  let refreshToken = "";
+  //check for existing token
+  const existingToken = await Token.findOne({ user: user._id });
+  if (existingToken) {
+    const { isValid } = existingToken;
+    if (!isValid) {
+      throw new CustomError.UnauthenticatedError("Invalid Credentials");
+    }
+    refreshToken = existingToken.refreshToken;
+    attachCookiesToResponse({ res, user: tokenUser, refreshToken });
+    res.status(StatusCodes.OK).json({ user: tokenUser });
+    return;
+  }
+
+  refreshToken = crypto.randomBytes(40).toString("hex");
+  const userAgent = req.headers["user-agent"];
+  const ip = req.ip;
+  const userToken = { refreshToken, userAgent, ip, user: user._id };
+  await Token.create(userToken);
+  attachCookiesToResponse({ res, user: tokenUser, refreshToken });
   res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
 const logout = async (req, res) => {
-  res.cookie("token", "logout", {
+  await Token.findOneAndDelete({ user: req.user.userId });
+
+  res.cookie("accessToken", "logout", {
     httpOnly: true,
-    expires: new Date(Date.now() + 5000),
+    expires: new Date(Date.now()),
+  });
+
+  res.cookie("refreshToken", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
   });
   res.status(StatusCodes.OK).json("User logged out");
 };
 
-module.exports = { register, login, logout };
+module.exports = { register, login, verifyEmail, logout };
