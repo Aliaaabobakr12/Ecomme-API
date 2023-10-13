1931bba94b3c001d69d7955395b7c6a082772498
Aliaaabobakr12 <aliaaabobakr12@gmail.com>
2023-10-13 13:33:59 +0300

    edit nodemailer config info

--- utils/nodemailerConfig.js
+++ utils/nodemailerConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   host: "smtp.ethereal.email",
   port: 587,
   auth: {
-    user: "shania.dickinson@ethereal.email",
-    pass: "xtkzf5aU15nBBWfUBj",
+    user: "jalyn.christiansen@ethereal.email",
+    pass: "eMxy6ga3hPK4C7D7mK",
   },
 };
