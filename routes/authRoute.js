9de37b9d50f6325571ec4038a00dbe3e5c92d881
Aliaaabobakr12 <aliaaabobakr12@gmail.com>
2023-10-13 13:35:10 +0300

    Add forgot and reset password routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,11 +7,15 @@ const {
   login,
   verifyEmail,
   logout,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/verify-email", verifyEmail);
 router.delete("/logout", authenticateUser, logout);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
